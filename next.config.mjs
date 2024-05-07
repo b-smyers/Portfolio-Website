9fd3dfee5576b2d99295b5def9a2fad178765234
Brendan Smyers <bs602422@ohio.edu>
2024-05-06 23:18:01 -0400

    new configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    basePath: '/portfolio-website',
+    assetPrefix: '/portfolio-website/'
 };
 
 export default nextConfig;
