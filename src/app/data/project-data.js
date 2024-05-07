39ec106b4287b1d36967bcc998cbe97b72bfc90f
Brendan Smyers <bs602422@ohio.edu>
2024-05-06 23:35:54 -0400

    Updated image paths for GitHub pages

--- src/app/data/project-data.js
+++ src/app/data/project-data.js
@@ -5,7 +5,7 @@ export const project_data = [{
     link: 'https://www.roblox.com/games/5195557959/Dungeon-Odyssey',
     timeFrame: '2020 - Present',
     brief: 'Dungeon Odyssey is a procedurally generated dungeon crawler where you and your party defeat monsters, collect loot, and delve deeper, uncovering the secrets of the dungeon on your journey.',
-    src: "/assets/images/DungeonOdyssey.png"
+    src: "./assets/images/DungeonOdyssey.png"
 },
 {
     id: 1,
@@ -14,7 +14,7 @@ export const project_data = [{
     link: 'https://github.com/OU-CS3560/s24-battlecode', // Private repo
     timeFrame: 'Jan 2024 - May 2024',
     brief: 'Battlecode is a competitive real time strategy (RTS) game developed by MIT, it which participants create a distributed AI system to control to battle against other competitors.',
-    src: "/assets/images/Battlecode-DefaultMedium.png"
+    src: "./assets/images/Battlecode-DefaultMedium.png"
 },
 {
     id: 2,
@@ -23,7 +23,7 @@ export const project_data = [{
     link: 'https://github.com/b-smyers/Boop-AI-Arena',
     timeFrame: 'December 2023',
     brief: 'Boop AI Arena is a terminal based extension on the final project for a computer science class. The program was created to allow anyone to create there own AI strategies for the game Boop (a tic tac toe like game), and be able to see how they compare to other AI.',
-    src: '/assets/images/Boop_AI_Arena.jpg'
+    src: './assets/images/Boop_AI_Arena.jpg'
 },
 {
     id: 3,
@@ -32,5 +32,5 @@ export const project_data = [{
     link: 'https://github.com/D-Mello/BridgeTrussSolver',
     timeFrame: 'December 2022',
     brief: 'The bridge truss solver was created using python and matplotlib to analyze and optimize bridge truss designs. The program can handle custom materials and their compressive and tensile strengths.',
-    src: '/assets/images/Genetic_Truss_Optimizer.png'
+    src: './assets/images/Genetic_Truss_Optimizer.png'
 }]
--- src/app/data/social-data.js
+++ src/app/data/social-data.js
@@ -2,17 +2,17 @@ export const social_data = [{
     id: 0,
     name: 'GitHub',
     link: 'https://github.com/b-smyers',
-    src: '/assets/icons/github.svg'
+    src: './assets/icons/github.svg'
 },
 {
     id: 1,
     name: 'LinkedIn',
     link: 'https://www.linkedin.com/in/b-smyers/',
-    src: '/assets/icons/linkedin.png'
+    src: './assets/icons/linkedin.png'
 },
 {
     id: 2,
     name: 'Outlook',
     link: 'mailto:bs602422@ohio.edu',
-    src: '/assets/icons/outlook.svg'
+    src: './assets/icons/outlook.svg'
 }]
