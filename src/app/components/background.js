9dafed6fff4f4c49bb529925b0a9a2b39b46f7f5
Brendan Smyers <bs602422@ohio.edu>
2024-05-07 23:17:18 -0400

    Fireworks scale with window size, cleaned up logic

--- src/app/components/background.js
+++ src/app/components/background.js
@@ -2,6 +2,7 @@
 
 import React, { useRef, useEffect } from 'react';
 import './background.css';
+import { Waiting_for_the_Sunrise } from 'next/font/google';
 
 class Particle {
     constructor(x, y, color) {
@@ -11,9 +12,9 @@ class Particle {
         this.radius = 2;
         this.dx = Math.random() * 4 - 2;
         this.dy = Math.random() * 4 - 2;
-        this.alpha = 1;
-        this.fadeRate = 0.02;
-        this.lifespan = 175;
+        this.gravity = 0.03;
+        this.alpha = 1; // also serves as death trigger
+        this.fadeRate = 0.01; 
         this.dead = false;
     }
 
@@ -22,10 +23,9 @@ class Particle {
 
         this.x += this.dx;
         this.y += this.dy;
-        this.dy += 0.05;
+        this.dy += this.gravity;
         this.alpha -= this.fadeRate;
-        this.lifespan--;
-        this.dead = (this.lifespan <= 0);
+        this.dead = (this.alpha <= 0);
     }
 
     draw(ctx) {
@@ -48,7 +48,7 @@ class Firework {
         this.radius = 3;
 
         // Firework explosion settings
-        this.explosion_particles = 40;
+        this.explosion_particles = 25;
         this.particles = [];
         this.exploding = false; // If the firework is in the exploding anim
         this.exploded = false; // If the exploding anim is over
@@ -60,16 +60,16 @@ class Firework {
         this.y += this.dy;
         this.dy += this.gravity;
 
-        if (this.dy >= 1 && !this.exploding) {
-            this.exploding = true;
+        if (this.dy <= 1 || this.exploding) { return; }
 
-            // Initialize particles
-            for (let i = 0; i < this.explosion_particles; i++) {
-                const color = [Math.random() * 255, Math.random() * 255, Math.random() * 255];
-                const particle = new Particle(this.x, this.y, color);
-                this.particles.push(particle);
-            }
+        this.exploding = true;
+        // Initialize particles
+        for (let i = 0; i < this.explosion_particles; i++) {
+            const color = [Math.random() * 255, Math.random() * 255, Math.random() * 255];
+            const particle = new Particle(this.x, this.y, color);
+            this.particles.push(particle);
         }
+        
     }
 
     draw(ctx) {
@@ -82,7 +82,7 @@ class Firework {
             // Remove dead particles from the array
             this.particles = this.particles.filter(particle => !particle.dead);
 
-            // Draw particles
+            // Update & Draw particles
             for (let i = 0; i < this.particles.length; i++) {
                 this.particles[i].update();
                 this.particles[i].draw(ctx);
@@ -97,49 +97,49 @@ class Firework {
 export default function Background() {
     const canvasRef = useRef(null);
     
-    let fireworks = [];
-    const max_fireworks = 24;
-
     useEffect(() => {
+        let fireworks = [];
+        let max_fireworks;
+        // Draw calls go here
+        const draw = (ctx, width, height) => {
+            ctx.clearRect(0, 0, width, height); // Clear the canvas before drawing
+            // Draw commands here
+            // ctx.fillStyle = '#000000';
+            // ctx.fillRect(0, 0, width, height);
+
+            // Remove spent fireworks from the array
+            fireworks = fireworks.filter(firework => !firework.exploded);
+            
+            // Refill fireworks
+            while(fireworks.length < max_fireworks) {
+                let firework =  new Firework(Math.random() * width, height);
+                fireworks.push(firework);
+            }
+            
+            // Update each firework
+            ctx.fillStyle = '#F0F0F0';
+            for (let i = 0; i < fireworks.length; i++) {
+                fireworks[i].update();
+                fireworks[i].draw(ctx);
+            } 
+        };
+
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
 
-        // Set canvas dimensions based on window size
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-
         // Draw loop
         const intervalId = setInterval(() => {
+            // Set canvas dimensions based on window size
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+            // Dynamically update max fireworks based on window size
+            max_fireworks = Math.floor(canvas.width / 75);
             draw(ctx, canvas.width, canvas.height);
         }, 10);
 
         // Clean up the interval when the component unmounts
         return () => clearInterval(intervalId);
     }, []);
-
-    // Draw calls go here
-    const draw = (ctx, width, height) => {
-        ctx.clearRect(0, 0, width, height); // Clear the canvas before drawing
-        // Draw commands here
-        // ctx.fillStyle = '#000000';
-        // ctx.fillRect(0, 0, width, height);
-
-        // Remove spent fireworks from the array
-        fireworks = fireworks.filter(firework => !firework.exploded);
-        
-        // Refill fireworks
-        while(fireworks.length < max_fireworks) {
-            let firework =  new Firework(Math.random() * width, height);
-            fireworks.push(firework);
-        }
-        
-        // Update each firework
-        ctx.fillStyle = '#F0F0F0';
-        for (let i = 0; i < fireworks.length; i++) {
-            fireworks[i].update();
-            fireworks[i].draw(ctx);
-        } 
-    };
 
     return <canvas ref={canvasRef} className='canvas'/>;
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,6 @@ import './page.css';
 export default function Page() {
     return (
         <div className='window flex-column flex-center-v flex-center-h'>
-            <h1 className='title padding-20'>Landing Page</h1>
             <div className='flex-10 flex-column flex-center-h flex-center-v'>
                 <h1 className='Name'>Brendan Smyers</h1>
                 <p>&quot;If opportunity doesn&apos;t knock, build a door&quot; - Milton Berle</p>
